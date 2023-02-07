07403627ae91d77846c1ddb33b1540c7d03e026d
weizhen94 <weizhen94@gmail.com>
2023-02-07 10:08:08 +0800

    first commit

---
+++ src/main/java/ibf2022/ssf/day12_workshop/Day12WorkshopApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.day12_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibf2022/ssf/day12_workshop/Day12WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.day12_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
